2a294a9937224386c6bff2a1c9464c4122734e84
sfifteen <mathias.seidler@s-fifteen.com>
2020-06-15 11:37:10 +0800

    fixed type

--- Settings_WebClient/app.py
+++ Settings_WebClient/app.py
@@ -1,7 +1,7 @@
 import dash
 import dash_bootstrap_components as dbc
 
-import S15QKD.controller as qkd_ctrl
+import S15qkd.controller as qkd_ctrl
 qkd_ctrl.start_communication()
 
 # app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
