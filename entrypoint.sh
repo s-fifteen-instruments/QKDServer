f31bc36561a51b125fd5f8f00a3788b9c58d60aa
Syed Abdullah Aljunid <syed.aljunid@gmail.com>
2022-06-17 16:21:17 +0800

    working qkd controller

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 cd /root/code/qcrypto/remotecrypto \
         && cd /root/code/QKDserver \
         && pip install -e . \
-	&& cd /root/code/QKDserver/S15qkd \
-	&& python3 authd.py & \
+#	&& cd /root/code/QKDserver/S15qkd \
+#	&& python3 authd.py & 
+#        cd /root/code/QKDserver/Settings_WebClient \
         cd /root/code/QKDserver/Settings_WebClient \
         && gunicorn --threads=1 -b 0.0.0.0:8000 index:server
 
