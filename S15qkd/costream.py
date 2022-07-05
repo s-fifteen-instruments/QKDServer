f196da63437c924cd9bc10135c27fc8a95633ede
pyuxiang <pehyuxiang@gmail.com>
2022-07-05 21:17:38 +0000

    Refactor costream

--- S15qkd/costream.py
+++ S15qkd/costream.py
@@ -26,162 +26,217 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-__author__ = 'Mathias Alexander Seidler'
-__copyright__ = 'Copyright 2020, S-Fifteen Instruments Pte. Ltd.'
-__credits__ = ['Lim Chin Chean']
-__license__ = 'MIT'
-__version__ = '0.0.1'
-__maintainer__ = 'Mathias Seidler'
-__email__ = 'mathias.seidler@s-fifteen.com'
-__status__ = 'dev'
+import pathlib
 
-
-# Built-in/Generic Imports
-import subprocess
-import os
-import time
-import threading
-import json
-import sys
-import psutil
-from types import SimpleNamespace
-
-# Own modules
-from . import qkd_globals
-from .rawkey_diagnosis import RawKeyDiagnosis
-from .qkd_globals import logger, QKDProtocol, PipesQKD, FoldersQKD, QKDEngineState
+from .utils import Process
+from .qkd_globals import logger, QKDProtocol, PipesQKD, FoldersQKD, QKDEngineState, config_file
 from .polarization_compensation import PolarizationDriftCompensation
-from . import controller, transferd
+from .rawkey_diagnosis import RawKeyDiagnosis
 
+# TODO(Justin): To eventually remove reference to global controller,
+#               when instances are created in the main control loop.
+from . import controller
+
+class Costream(Process):
+
+    def __init__(self, program):
+        super().__init__(program)
+        self._latest_coincidences = -1
+        self._latest_accidentals = -1
+        self._latest_deltat = -1
+        self._initial_time_difference = None
+        self._latest_compress = ''
+        self._latest_sentevents = ''
+        self._latest_rawevents = ''
+        self._latest_outepoch = ''
+        self._protocol = None
+
+        # Polarization compensation
+        self._polarization_compensator = None
+        self._pairs_over_accidentals_avg = 10
+        self._callback_restart = None
+    
+    def start(
+            self,
+            time_difference: int,
+            begin_epoch: str,
+            qkd_protocol=QKDProtocol.BBM92,
+            callback_restart=None,  # message passing back to controller to restart keygen,
+            callback_servicemode=None,  # message passing to mark as service mode
+        ):
+        assert not self.is_running()
+
+        self._initial_time_difference = time_difference
+        self._protocol = qkd_protocol
+        self._callback_restart = callback_restart
+        self._callback_servicemode = callback_servicemode
+        
+        self.read(PipesQKD.GENLOG, self.digest_genlog, 'GENLOG')
+
+        args = [
+            '-d', FoldersQKD.RECEIVEFILES,
+            '-D', FoldersQKD.T1FILES,
+            '-f', FoldersQKD.RAWKEYS,
+            '-F', FoldersQKD.SENDFILES,
+            '-e', f'0x{begin_epoch}',
+            Process.config.kill_option,
+            '-t', time_difference,
+            '-p', qkd_protocol,
+            '-T', 2,
+            '-m', f'/{Process.config.data_root}/rawpacketindex',
+            '-M', PipesQKD.CMD,
+            '-n', PipesQKD.GENLOG,
+            '-V', 5,
+            '-G', 2,
+            '-w', Process.config.remote_coincidence_window,
+            '-u', Process.config.tracking_window,
+            '-Q', int(-config.track_filter_time_constant),
+            '-R', 5,
+            Process.config.costream_histo_option,
+            '-h', config.costream_histo_number,
+        ]
+        logger.info(f'costream starts with the following arguments: {args}')
+        super().start(args, stderr="costreamerror")
+
+        # Enable polarization compensation
+        if Process.config.do_polarization_compensation is True \
+                and qkd_protocol == QKDProtocol.SERVICE:
+            self._polarization_compensator = PolarizationDriftCompensation(
+                Process.config.LCR_polarization_compensator_path,
+            )
+
+
+    def digest_genlog(self, pipe):
+        """Digests the genlog pipe written by costream."""
+        message = pipe.readline().decode().rstrip('\n').lstrip('\x00')
+        if len(message) == 0:
+            return
+
+        logger.debug(message)
+        (
+            self._latest_outepoch,
+            self._latest_rawevents,
+            self._latest_sentevents,
+            self._latest_compress,
+            self._latest_deltat,
+            self._latest_accidentals,
+            self._latest_coincidences,
+            *_,
+         ) = message.split()  # costream_info
+
+        # restart time difference finder if pairs to accidentals is too low
+        pairs_over_accidentals = int(latest_coincidences) / (int(latest_accidentals) + 1) #incase of divide by zero
+        self._pairs_over_accidentals_avg = (self._pairs_over_accidentals_avg * 19 + pairs_over_accidentals) / 20
+        if self._pairs_over_accidentals_avg < 2.5:
+            logger.error(
+                f'Pairs to accidental ratio bad: avg(p/a) = {self._pairs_over_accidentals_avg:.2f}'
+            )
+
+            # Be careful of potential race condition.
+            # In current implementation, termination condition for this thread
+            # evaluated only after this function returns, so no conflict.
+            self._callback_restart()
+            return
+
+        # If in SERVICE mode, mark as SERVICE mode in QKD engine
+        if qkd_protocol == QKDProtocol.SERVICE:
+            self._callback_servicemode()
+            logger.debug(message)
+            diagnosis = RawKeyDiagnosis(
+                pathlib.Path(FoldersQKD.RAWKEYS) / message
+            )
+            logger.debug(
+                f'Service mode, QBER: {diagnosis.quantum_bit_error}, Epoch: {self._latest_outepoch}'
+            )
+
+            # Perform polarization compensation while still in SERVICE mode
+            if self._polarization_compensator and pairs_over_accidentals > 2.5:
+                self._polarization_compensator.update_QBER(
+                    diagnosis.quantum_bit_error, epoch=message,
+                )
+
+    # Coding defensively... ensure these properties are not
+    # modified outside class.
+
+    @property
+    def latest_coincidences(self):
+        return self._latest_coincidences
+    
+    @property
+    def latest_accidentals(self):
+        return self._latest_accidentals
+    
+    @property
+    def latest_deltat(self):
+        return self._latest_deltat
+
+    @property
+    def initial_time_difference(self):
+        return self._initial_time_difference
+    
+    @property
+    def latest_compress(self):
+        return self._latest_compress
+    
+    @property
+    def latest_sentevents(self):
+        return self._latest_sentevents
+    
+    @property
+    def latest_rawevents(self):
+        return self._latest_rawevents
+    
+    @property
+    def latest_outepoch(self):
+        return self._latest_outepoch
+    
+    @property
+    def protocol(self):
+        return self._protocol
+
+
+# Wrapper
+Process.load_config()
+costream = Costream(Process.config.program_root + '/costream')
+
+def callback_restart():
+    """Callback to controller to restart keygen."""
+    protocol = costream.protocol
+    # TODO(Justin): Abstract into single command to controller, e.g.
+    #               controller.restart_keygen(protocol)
+    controller.stop_key_gen()
+    if qkd_protocol == QKDProtocol.SERVICE:
+        controller.start_service_mode()
+    elif qkd_protocol == QKDProtocol.BBM92:
+        controller.start_key_generation()
+
+# Conflicting states between qkd_protocol and controller.qkd_engine_state...
+def callback_servicemode():
+    controller.qkd_engine_state = QKDEngineState.SERVICE_MODE
+
+# Original interface
 proc_costream = None
 
-
-def _initialize(config_file_name: str = qkd_globals.config_file):
+def start_costream(
+        time_difference: int,
+        begin_epoch: str,
+        qkd_protocol: int = QKDProtocol.BBM92,
+        config_file_name: str = config_file,
+    ):
     global proc_costream
-    global latest_coincidences, latest_accidentals, latest_deltat, latest_sentevents
-    global latest_compress, latest_rawevents, latest_outepoch, initial_time_difference
-
-    proc_costream = None
-    latest_coincidences = -1
-    latest_accidentals = -1
-    latest_deltat = -1
-    initial_time_difference = None
-    latest_compress = ''
-    latest_sentevents = ''
-    latest_rawevents = ''
-    latest_outepoch = ''
-
-
-def start_costream(time_difference: int,
-                   begin_epoch: str,
-                   qkd_protocol: int = QKDProtocol.BBM92,
-                   config_file_name: str = qkd_globals.config_file):
-    global proc_costream, initial_time_difference, protocol
-    initial_time_difference = time_difference
-    with open(config_file_name, 'r') as f:
-        config = json.load(f, object_hook=lambda d: SimpleNamespace(**d))
-    _initialize()
-    protocol = qkd_protocol
-    logger.info(f'{begin_epoch}')
-    args = f'-d {FoldersQKD.RECEIVEFILES} \
-             -D {FoldersQKD.T1FILES} \
-             -f {FoldersQKD.RAWKEYS} \
-             -F {FoldersQKD.SENDFILES} \
-             -e 0x{begin_epoch} \
-             {config.kill_option} \
-             -t {time_difference} \
-             -p {qkd_protocol} \
-             -T 2 \
-             -m /{config.data_root}/rawpacketindex \
-             -M {PipesQKD.CMD} \
-             -n {PipesQKD.GENLOG} \
-             -V 5 \
-             -G 2 -w {config.remote_coincidence_window} \
-             -u {config.tracking_window} \
-             -Q {int(-config.track_filter_time_constant)} \
-             -R 5 \
-             {config.costream_histo_option} \
-             -h {config.costream_histo_number}'
-    program_costream = config.program_root + '/costream'
-    logger.info(f'costream starts with the following arguments: {args}')
-    with open(f'/{config.data_root}/costreamerror', 'a+') as f:
-        proc_costream = subprocess.Popen((program_costream, *args.split()),
-                                         stdout=subprocess.PIPE, stderr=f)
-    costream_thread = threading.Thread(
-        target=_genlog_digest, args=([qkd_protocol]))
-    costream_thread.start()
-
-
-def _genlog_digest(qkd_protocol, config_file_name: str = qkd_globals.config_file):
-    '''
-    Digests the genlog pipe written by costream.
-    '''
-    global latest_coincidences, latest_accidentals, latest_deltat, latest_sentevents
-    global latest_compress, latest_rawevents, latest_outepoch
-    fd = os.open(PipesQKD.GENLOG, os.O_RDONLY | os.O_NONBLOCK)
-    f = os.fdopen(fd, 'rb', 0)  # non-blocking
-    logger.info('Thread started.')
-    with open(config_file_name, 'r') as f_config:
-        config = json.load(f_config, object_hook=lambda d: SimpleNamespace(**d))
-    if config.do_polarization_compensation is True and qkd_protocol == QKDProtocol.SERVICE:
-        polarization_compensator = PolarizationDriftCompensation(
-            config.LCR_polarization_compensator_path)
-    pairs_over_accidentals_avg = 10
-    while is_running():
-        time.sleep(0.1)
-        try:
-            message = (f.readline().decode().rstrip('\n')).lstrip('\x00')
-            if len(message) != 0:
-                logger.debug(message)
-                costream_info = message.split()
-                latest_coincidences = costream_info[6]
-                latest_accidentals = costream_info[5]
-                latest_deltat = costream_info[4]
-                latest_compress = costream_info[3]
-                latest_sentevents = costream_info[2]
-                latest_rawevents = costream_info[1]
-                latest_outepoch = costream_info[0]
-                # restart time difference finder if pairs to accidentals is too low
-                pairs_over_accidentals = int(
-                    latest_coincidences) / (int(latest_accidentals) + 1) #incase of divide by zero
-                pairs_over_accidentals_avg = (pairs_over_accidentals_avg * 19 + pairs_over_accidentals) / 20
-                if pairs_over_accidentals_avg < 2.5:
-                    logger.error(
-                        f'Pairs to accidental ratio bad: avg(p/a) = {pairs_over_accidentals_avg:.2f}')
-                    controller.stop_key_gen()
-                    if qkd_protocol == QKDProtocol.SERVICE:
-                        controller.start_service_mode()
-                    elif qkd_protocol == QKDProtocol.BBM92:
-                        controller.start_key_generation()
-                    continue
-                if qkd_protocol == QKDProtocol.SERVICE:
-                    controller.qkd_engine_state = QKDEngineState.SERVICE_MODE
-                    logger.debug(message)
-                    diagnosis = RawKeyDiagnosis(
-                        FoldersQKD.RAWKEYS + '/' + message)
-                    logger.debug(
-                        f'Service mode, QBER: {diagnosis.quantum_bit_error}, Epoch: {costream_info[0]}')
-                    if config.do_polarization_compensation is True:
-                        if pairs_over_accidentals > 2.5:
-                            #polarization_compensator.update_qber2(diagnosis.quantum_bit_error)
-                            polarization_compensator.update_QBER(
-                            diagnosis.quantum_bit_error, epoch=message)
-
-        except OSError:
-            pass
-        except Exception as a:
-            logger.error(a)
-    logger.info('Thread finished.')
-
+    Process.load_config(config_file_name)
+    costream.start(time_difference, begin_epoch, qkd_protocol, callback_restart, callback_servicemode)
+    proc_costream = costream.process
 
 def stop_costream():
     global proc_costream
-    qkd_globals.kill_process(proc_costream)
+    costream.stop()
     proc_costream = None
 
-
 def is_running():
-    return not (proc_costream is None or proc_costream.poll() is not None)
+    return costream.is_running()
 
-
-_initialize()
+# Exposes class properties as global variables
+# using module-level __getattr__ available in Python 3.7+
+def __getattr__(name):
+    return getattr(costream, name)
