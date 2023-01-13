4f96e759353e26e29b12a43fa2e6048e9a43cfa2
Syed Abdullah Aljunid <syed.aljunid@gmail.com>
2023-01-13 19:03:19 +0800

    Removed unused files

--- S15qkd/test_sb.py
+++
@@ -1,8 +0,0 @@
-from S15qkd.utils import Process
-from S15qkd import readevents
-import pathlib
-Process.load_config()
-Process.config.program_root = '/root/programs/qcrypto/remotecrypto'
-dir_qcrypto = pathlib.Path(Process.config.program_root)
-re = readevents.Readevents(dir_qcrypto / 'readevents')
-re.start_sb()
